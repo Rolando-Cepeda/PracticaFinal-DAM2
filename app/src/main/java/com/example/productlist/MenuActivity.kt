36499c9a7f958e957194230a2891aa07ff270d21
Rolando_cf <rolandodj83@gmail.com>
2024-07-14 22:11:38 +0200

    Hemos implementado el viewBinding y creado la funci{on initUI, la misma que buscará en la API

--- app/src/main/java/com/example/productlist/MenuActivity.kt
+++ app/src/main/java/com/example/productlist/MenuActivity.kt
@@ -6,14 +6,15 @@ import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class MenuActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
-val botonInicio = findViewById<Button>(R.id.btnInicio)
-        botonInicio.setOnClickListener { navigateToProductInicio()}
+        val botonInicio = findViewById<Button>(R.id.btnInicio)
+        botonInicio.setOnClickListener { navigateToProductInicio() }
     }
 
-    private fun navigateToProductInicio(){
+    private fun navigateToProductInicio() {
         val intent = Intent(this, ProductListActivity::class.java)
         startActivity((intent))
     }
--- app/src/main/java/com/example/productlist/ProductListActivity.kt
+++ app/src/main/java/com/example/productlist/ProductListActivity.kt
@@ -2,11 +2,39 @@ package com.example.productlist
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView
+import com.example.productlist.databinding.ActivityProductListBinding
+
 
 class ProductListActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityProductListBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_product_list)
+        binding = ActivityProductListBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initUI()
+    }
+
+    private fun initUI() {
+        //Cuando yo escriba y pulso el BOTón de buscar ahi hará la llamada
+        binding.searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            //Esta función se activará cuando demos al botón de buscar MIN 16:31
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                searchByName(query.orEmpty())
+                return false
+            }
+
+            // Esta se llamará, cada vez que vayamos escribiendo, pero no la utlizaremos.
+            override fun onQueryTextChange(newText: String?): Boolean {
+                return false
+            }
+        })
+    }
+
+    //Esta función nos devolverá el contenido de la API
+    private fun searchByName(query: String) {
 
     }
 }
