b7419fa0f8d3b32286077b78d7d4bcb9699a9276
Rolando_cf <rolandodj83@gmail.com>
2024-07-14 20:28:06 +0200

    Creamos una pantalla de Inicio MenuActivity.kt que nos redigirá a otra pantalla de búsqueda ProductListActivity.kt.

---
+++ app/src/main/java/com/example/productlist/MenuActivity.kt
@@ -0,0 +1,20 @@
+package com.example.productlist
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class MenuActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu)
+val botonInicio = findViewById<Button>(R.id.btnInicio)
+        botonInicio.setOnClickListener { navigateToProductInicio()}
+    }
+
+    private fun navigateToProductInicio(){
+        val intent = Intent(this, ProductListActivity::class.java)
+        startActivity((intent))
+    }
+}
---
+++ app/src/main/java/com/example/productlist/ProductListActivity.kt
@@ -0,0 +1,12 @@
+package com.example.productlist
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ProductListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_product_list)
+
+    }
+}
