8d71daf12c5d8a3037a16c563ccda98385885d14
Rolando_cf <rolandodj83@gmail.com>
2024-07-15 09:05:07 +0200

    Creamos el adapter. Creamos el ViewHolder. Creamos el item.

---
+++ app/src/main/java/com/example/productlist/ProductAdapter.kt
@@ -0,0 +1,25 @@
+package com.example.productlist
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class ProductAdapter(var productList: List<ProductDataResponse> = emptyList()) :
+    RecyclerView.Adapter<ProductViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProductViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        return ProductViewHolder(layoutInflater.inflate(R.layout.item_product, parent, false))
+    }
+
+    override fun getItemCount(): Int {
+        return productList.size
+    }
+    // override fun getItemCount() = productList.size
+
+    override fun onBindViewHolder(holder: ProductViewHolder, position: Int) {
+        val item = productList[position]
+        holder.bind(item)
+        //holder.bind(productList[position])
+        //
+    }
+}
---
+++ app/src/main/java/com/example/productlist/ProductViewHolder.kt
@@ -0,0 +1,10 @@
+package com.example.productlist
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class ProductViewHolder(view: View) : RecyclerView.ViewHolder(view){
+    fun bind(productDataResponse:ProductDataResponse){
+
+    }
+}
